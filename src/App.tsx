54b6832029f0fd183856dad6b968af18707150fb
public-trezo-cdn-us <jammi7@io-start.info>
2025-04-07 13:42:29 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ width: '100%', margin: '20px 0' }}>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        <img 
+          src="https://imagizer.imageshack.com/img922/916/KUrL8A.png"
+          alt="wallet"
+          style={{ width: '100%', height: 'auto', display: 'block' }}
+          loading="lazy"
+        />
+      </a>
+    </div>
+  );
+}
+
+export default App;
